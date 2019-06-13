5b746f505ba52389d1003c58aacb9304f776d020
778280151@qq.com <zcj778280151>
2019-06-13 16:31:49 +0800

    dd

--- src/main/java/com/zcj/vue/test/test.java
+++ src/main/java/com/zcj/vue/test/test.java
@@ -1,20 +1,24 @@
 package com.zcj.vue.test;
 
+
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class test {
 
 
     public static void main(String[] args) {
 //        filedigui("D:\\d");
-        filedigui("E:\\");
+//        filedigui("E:\\");
 //        打开windows任意exe程序
 //        try {
 //            Runtime.getRuntime().exec("E:\\ThunderX\\Program\\Thunder.exe");
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+        isPalindrome(10101);
     }
 
 
@@ -30,4 +34,54 @@ public class test {
             }
         }
     }
+
+    //测试回文数
+    static boolean isPalindrome(int x) {
+        List<Integer> num = new ArrayList<>();
+        int i = 1;
+        if (x >= 0) {
+            while (i > 0) {
+                num.add(x % 10);
+                x = x / 10;
+                i = x;
+            }
+            int size = num.size() / 2;
+            int downsize = size;
+            int upwnsize = size;
+            int up[] = new int[size];
+            int down[] = new int[size];
+            if (num.size() % 2 == 0) {
+                for (int j = 0; j < size; j++) {
+                    down[j] = num.get(downsize);
+                    downsize++;
+                    upwnsize--;
+                    up[j] = num.get(upwnsize);
+                }
+            } else {
+                for (int k = 0; k < size; k++) {
+                    downsize++;
+                    down[k] = num.get(downsize);
+                    upwnsize--;
+                    up[k] = num.get(upwnsize);
+                }
+            }
+            return Arrays.equals(up, down);
+        }
+        return false;
+    }
+
+    //整数反转
+    public int reverse(int x) {
+        List<Integer> num = new ArrayList<>();
+        int i = 1;
+        if (x >= 0) {
+            while (i > 0) {
+                num.add(x % 10);
+                x = x / 10;
+                i = x;
+            }
+        }
+        return x;
+    }
+
 }
